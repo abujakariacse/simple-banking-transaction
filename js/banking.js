ca810769f14839f192e6de6cf9f43af72c613a65
abujakariacse <abujakaria316@gmail.com>
2022-02-15 07:39:31 +0600

    arror throw

--- js/banking.js
+++ js/banking.js
@@ -16,11 +16,19 @@ function updateTotalField(totalInputField,amount){
     getCurrentdepowith.innerText = amount + currentDepoWith;
 }
 
-function updateBalance(amount,isAdd){
+function presentBalance(){
     const getCurrentBalance = document.getElementById('current-balance');
     const currentBalanceText = getCurrentBalance.innerText;
     const currentBalance = parseFloat(currentBalanceText);
-  
+    return currentBalance;
+}
+
+function updateBalance(amount,isAdd){
+    const getCurrentBalance = document.getElementById('current-balance');
+    // const currentBalanceText = getCurrentBalance.innerText;
+    // const currentBalance = parseFloat(currentBalanceText);
+    const currentBalance = presentBalance();
+
     if(isAdd){
     getCurrentBalance.innerText = amount + currentBalance;
    }
@@ -32,15 +40,30 @@ function updateBalance(amount,isAdd){
 
 document.getElementById('deposit-btn').addEventListener('click',function(){
     const depositAmount = getInputValue('deposit-field');
-    updateTotalField('current-deposit',depositAmount);
-    updateBalance(depositAmount,true);
+   
+    if(depositAmount > 0){
+        updateTotalField('current-deposit',depositAmount);
+        updateBalance(depositAmount,true);
+    }
+    else if(depositAmount != 'number'){
+        const errorThrow = document.getElementById('deposit-field');
+        errorThrow.style.border = '2px solid red';
+        
+    }
     
 })
 
 
 document.getElementById('withdraw-btn').addEventListener('click',function(){
     const withdrawAmount = getInputValue('withdraw-field');
-    updateTotalField('current-withdraw',withdrawAmount);
-    updateBalance(withdrawAmount,false);
-    
+    const currentBalance = presentBalance();
+    if(withdrawAmount > 0 && withdrawAmount < currentBalance){
+        updateTotalField('current-withdraw',withdrawAmount);
+        updateBalance(withdrawAmount,false);
+    }
+    else if(withdrawAmount != 'number'){
+        const errorThrow = document.getElementById('withdraw-field');
+        errorThrow.style.border = '2px solid red';
+        
+    }
 })
