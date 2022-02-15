f2956bcb56b0247a7af15187780a0c17593fe806
abujakariacse <abujakaria316@gmail.com>
2022-02-15 07:10:31 +0600

    duplicate of organize

--- js/banking.js
+++ js/banking.js
@@ -1,57 +1,46 @@
-document.getElementById('deposit-btn').addEventListener('click',function(){
-    //Get deposit input 
-    const depositInput = document.getElementById('deposit-field');
-    const depositInputText = depositInput.value;
-    const depositAmount = parseFloat(depositInputText);
 
-    // Get current deposit amount
-    const getCurrentDeposit = document.getElementById('current-deposit');
-    const currentDepositText = getCurrentDeposit.innerText;
-    const currentDeposit = parseFloat(currentDepositText);
+function getInputValue(inputId) {
 
-    // Total Deposit & deposit innerText Set
-    const totalDeposit = depositAmount + currentDeposit;
-    getCurrentDeposit.innerText = totalDeposit;
+    const input = document.getElementById(inputId);
+    const inputText = input.value;
+    const amount = parseFloat(inputText);
+    input.value = '';
+    return amount;
 
-    depositInput.value = '';
+}
 
-    // Get Current Balance
+function updateTotalField(totalInputField,amount){
+    const getCurrentdepowith = document.getElementById(totalInputField);
+    const currentDepoWithText = getCurrentdepowith.innerText;
+    const currentDepoWith = parseFloat(currentDepoWithText);
+    getCurrentdepowith.innerText = amount + currentDepoWith;
+}
+
+function updateBalance(amount,isAdd){
     const getCurrentBalance = document.getElementById('current-balance');
     const currentBalanceText = getCurrentBalance.innerText;
     const currentBalance = parseFloat(currentBalanceText);
+  
+    if(isAdd){
+    getCurrentBalance.innerText = amount + currentBalance;
+   }
 
-    // New Balance Set
-    const newBalance = depositAmount + currentBalance;
-    getCurrentBalance.innerText = newBalance;
+   else{
+    getCurrentBalance.innerText = currentBalance - amount;
+   }
+}
 
-
+document.getElementById('deposit-btn').addEventListener('click',function(){
+    const depositAmount = getInputValue('deposit-field');
+    updateTotalField('current-deposit',depositAmount);
+    updateBalance(depositAmount,true);
+    
 })
 
 
 document.getElementById('withdraw-btn').addEventListener('click',function(){
-    //Get Withdraw Input 
-    const withdrawInput = document.getElementById('withdraw-field');
-    const withdrawInputText = withdrawInput.value;
-    const withdrawAmount = parseFloat(withdrawInputText);
-
-    // Get Current Withdrawal Amount
-    const getCurrentWithdraw = document.getElementById('current-withdraw');
-    const currentWithdrawText = getCurrentWithdraw.innerText;
-    const currentWithdraw = parseFloat(currentWithdrawText);
+    const withdrawAmount = getInputValue('withdraw-field');
+    updateTotalField('current-withdraw',withdrawAmount);
+    updateBalance(withdrawAmount,false);
     
-
-    // Total withdraw & total withdraw innerText Set
-    const totalWithdraw = withdrawAmount + currentWithdraw;
-    getCurrentWithdraw.innerText = totalWithdraw;
-
-    withdrawInput.value = '';
-
-    // Get Current Balance
-    const getCurrentBalance = document.getElementById('current-balance');
-    const currentBalanceText = getCurrentBalance.innerText;
-    const currentBalance = parseFloat(currentBalanceText);
-
-    // New Balance Set
-    const newBalance = currentBalance-withdrawAmount ;
-    getCurrentBalance.innerText = newBalance;
 })
